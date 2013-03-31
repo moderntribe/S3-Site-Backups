aa6c825825b8408f910afc82cb3dd2cdfd159c0c
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-03-30 22:59:29 -0400

    update the example config

--- config.inc.example.php
+++ config.inc.example.php
@@ -6,8 +6,11 @@ $archive_path = '/path/'; // This is where we're going to put our local backups
 $expire_after = 15; // How many days should Amazon hold on to a backup?
 $notify_email = 'email@goes.here'; // Comma-separated list of email address to notify on successful backup
 $notify_sitename = 'mysite.com'; // Name to use for email notification subject line
-$date = date("Y-m-d");
-$path_to_archive = '/path/to/archive/files/'; // The local path we want to back up. Leave it empty if you don't want to back files up.
+$date_format = "Y-m-d"; // Format for the date string in archive file names
+$paths_to_archive = array('/path/to/archive/files/'); // The local paths we want to back up. Leave it empty if you don't want to back files up.
+$archive_exclude_patterns = array('cache'); // Patterns to be excluded from the archive (e.g., to exclude cache files)
+$incremental = false; // Set to true to only archive files modified since the last full backup
+
 
 // Database
 
@@ -22,4 +25,3 @@ $amazon_key = '';
 $amazon_secret = '';
 $bucket = '';
 
-?>
